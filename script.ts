50adcaf19696608e6483d15b7780c1dd8f77b915
Abdul Basit Butt <abdul.butt@mths.ca>
2022-05-13 21:46:19 -0400

    Create script.ts

---
+++ script.ts
@@ -0,0 +1,25 @@
+/*
+ * This is Board Program
+ *
+ * @author  Abdul Basit Butt
+ * @version 1.0
+ * @since   2022-02-19
+ */
+
+import promptSync from "prompt-sync"
+const prompt = promptSync()
+
+function tipccalc (){
+    // Input
+    const billString = ("Enter your bill")
+    const bill = parseInt(billString)
+    const tipString = prompt("Enter the percentage of how much tip you want to give")
+    const tip = parseInt(tipString)
+    
+    // Calculatiions
+    var tipValue = bill * (tip/100)
+    var finalBill = bill + tipValue
+    console.log("Your total bill is " + finalBill + "")
+
+}
+tipccalc()
